7c5aa6d849a232127fecee3a0c6c81ad990598c8
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-07-03 23:03:19 +0100

    linkenList algorithm

---
+++ exp/linked-list.js
@@ -0,0 +1,16 @@
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
+
+class LinkedList {
+	constructor(value) {
+		this.head = new Node(value);
+		this.tail = this.head;
+		this.length = 1;
+	}
+}
+
+const myLinkedList = new LinkedList(10);
