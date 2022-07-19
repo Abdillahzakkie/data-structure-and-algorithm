419cf9f1d6bfbf59975c08e1c5a434a0b95807ae
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-07-19 09:00:58 +0100

    created Queue data type

---
+++ exp/queue.js
@@ -0,0 +1,17 @@
+/* A Node is a value and a pointer to the next Node. */
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
+
+/* We create a class called Queue, which has a constructor that sets the first, last, and length
+properties to null, and has methods called peek, enqueue, dequeue, and print */
+class Queue {
+	constructor() {
+		this.first = null;
+		this.last = null;
+		this.length = 0;
+	}
+}
