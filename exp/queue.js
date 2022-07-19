387932d76436fe1f820730f9926eca1f132bc2c7
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-07-19 09:04:26 +0100

    enqueue method add new item to the end of the queue

--- exp/queue.js
+++ exp/queue.js
@@ -22,4 +22,23 @@ class Queue {
 	peek() {
 		return this.first;
 	}
+
+	/**
+	 * If the queue is empty, set the first and last nodes to the new node, otherwise set the last node's
+	 * next property to the new node and set the last node to the new node.
+	 * @param value - The value to be added to the queue.
+	 * @returns The length of the queue.
+	 */
+	enqueue(value) {
+		const _newNode = new Node(value);
+		if (this.length === 0) {
+			this.first = _newNode;
+			this.last = this.first;
+			this.length++;
+			return this.length;
+		}
+		this.last.next = _newNode;
+		this.last = _newNode;
+		this.length++;
+	}
 }
