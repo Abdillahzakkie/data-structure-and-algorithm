904e13801aa601254ebab23c13300964c503d739
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-07-22 06:40:35 +0100

    Added insert
    Insert methods inserts new value into the binary tree.

--- exp/binarySearchTree.js
+++ exp/binarySearchTree.js
@@ -13,4 +13,30 @@ class BinarySearchTree {
 	constructor() {
 		this.root = null;
 	}
+
+	insert(_value) {
+		const _newNode = new Node(_value);
+		if (this.root === null) {
+			this.root = _newNode;
+			return this;
+		}
+
+		let _currentNode = this.root;
+		while (true) {
+			if (_value < _currentNode.value) {
+				if (!_currentNode.left) {
+					_currentNode.left = _newNode;
+					break;
+				}
+				_currentNode = _currentNode.left;
+			} else {
+				if (!_currentNode.right) {
+					_currentNode.right = _newNode;
+					break;
+				}
+				_currentNode = _currentNode.right;
+			}
+		}
+		return this;
+	}
 }
